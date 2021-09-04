ae535547a9d925eba67163f94fa10ddfc63cdc4c
StylistMercurial1130 <wrk.krthik@gmail.com>
2021-09-04 18:48:22 +0530

    commit to master

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include "Display.h"
+#include "Controller.h"
+#include "Game.h"
+#include "SDL2/SDL.h"
 
 using namespace std;
 
-int main(void){
-    cout<<"hello world !"<<endl;
+#define WINDOWHEIGHT 480
+#define WINDOWWIDTH 640
+#define RESOLUTION 10
+
+int main(int argc, char *argv[]){
+
+    int * gameworld = new int[(WINDOWHEIGHT/RESOLUTION) * (WINDOWWIDTH/RESOLUTION)];
+    if(SDL_Init(SDL_INIT_VIDEO) != 0){
+
+        SDL_Log("SDL Could not be initalized !");
+        return EXIT_FAILURE;
+
+    }
+
+    Display display = Display(WINDOWHEIGHT,WINDOWWIDTH,"Test window");
+    Controller controller = Controller();
+    Game game = Game(WINDOWHEIGHT,WINDOWWIDTH,RESOLUTION);
+    gameworld = game.Get_GameWorldBuffer();
+    display.InitBufferSurface(gameworld,(WINDOWWIDTH/RESOLUTION),(WINDOWHEIGHT/RESOLUTION));
+
+    while(controller.IsApplicationOpen()){
+
+        controller.PollInput();
+        gameworld[
+                    controller.GetMouseXPosition() / RESOLUTION + 
+                    ((controller.GetMouseYPosition() / RESOLUTION) * 64)
+                ] = 0xffffffff;
+
+        display.Draw(gameworld);
+    }
+
+    delete gameworld;
+    SDL_Quit();
+
+    return 0;
+
+
 }
