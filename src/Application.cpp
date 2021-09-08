66d41f4b3d80c2aeb9525142b0fcc1250cffcb6b
StylistMercurial1130 <wrk.krthik@gmail.com>
2021-09-08 20:31:19 +0530

    commit to game

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,1 +1,43 @@
 #include "Application.h"
+
+#pragma region Application
+
+void Application :: Transition(State * state){
+
+    if(m_State != nullptr)
+        delete m_State;
+    this->m_State = state;
+    this->m_State->SetContext(this);
+
+}
+
+Application :: Application(State * state) : m_State(nullptr){
+
+    this->m_State = state;
+
+}
+
+
+Application :: ~Application(){
+
+    delete m_State;
+
+}
+
+void State :: SetContext(Application * application){ this->m_Application = application; }
+
+#pragma endregion Application
+
+#pragma region ApplicationStates
+
+class Enter : public State{};
+
+class Pause : public State{};
+
+class Exit : public State{};
+
+class Run : public State{};
+
+
+#pragma endregion ApplicationStates
+
--- src/Application.h
+++ src/Application.h
@@ -1,1 +1,44 @@
+/**
+ * @file Application.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
 #pragma once
+
+#include "Game.h"
+#include "Display.h"
+
+class Application;
+
+class State{
+
+protected : 
+
+    Application * m_Application;
+
+public : 
+
+
+
+};
+
+class Application{
+
+private : 
+
+    State * m_State;
+
+public : 
+
+    Application(State * state);
+    void Transition(State * state);
+    ~Application();
+
+};
