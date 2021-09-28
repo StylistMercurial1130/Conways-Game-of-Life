1aad9b2d3a6edbb9793e39149f77fa50af47c1f2
StylistMercurial1130 <wrk.krthik@gmail.com>
2021-09-28 17:38:50 +0530

    commit to master

--- src/main.cpp
+++ src/main.cpp
@@ -30,36 +30,4 @@ int main(int argc, char *argv[]){
 
 
 
-/*
-    int * gameworld = new int[(WINDOWHEIGHT/RESOLUTION) * (WINDOWWIDTH/RESOLUTION)];
-    if(SDL_Init(SDL_INIT_VIDEO) != 0){
-
-        SDL_Log("SDL Could not be initalized !");
-        return EXIT_FAILURE;
-
-    }
-
-    Display display = Display(WINDOWHEIGHT,WINDOWWIDTH,"Test window");
-    Controller controller = Controller();
-    Game game = Game(WINDOWHEIGHT,WINDOWWIDTH,RESOLUTION);
-    gameworld = game.Get_GameWorldBuffer();
-    display.InitBufferSurface(gameworld,(WINDOWWIDTH/RESOLUTION),(WINDOWHEIGHT/RESOLUTION));
-
-    while(controller.IsApplicationOpen()){
-
-        controller.PollInput();
-        gameworld[
-                    controller.GetMouseXPosition() / RESOLUTION + 
-                    ((controller.GetMouseYPosition() / RESOLUTION) * 64)
-                ] = 0xffffffff;
-
-        display.Draw(gameworld);
-    }
-
-    delete gameworld;
-    SDL_Quit();
-
-    return 0;
-
-*/
 
